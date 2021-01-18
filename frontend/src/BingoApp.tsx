089482371e1ad4c9600ccd174ffccd33bdfb7e03
Joshua Prince <joshua@jtprince.com>
2021-01-18 00:03:31 -0800

    Ready for dev

---
+++ frontend/src/BingoApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BingoApp.css';
+import BoardPrimary from "./BoardPrimary";
+
+function BingoApp() {
+  return (
+    <>
+      <BoardPrimary/>
+    </>
+  );
+}
+
+export default BingoApp;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BingoApp from './BingoApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BingoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );

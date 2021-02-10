58244e948844ba97ea8762b209dc90b9808dda1f
Joshua Prince <joshua@jtprince.com>
2021-02-09 22:21:39 -0800

    Grid CSS

--- frontend/src/component/Board.tsx
+++ frontend/src/component/Board.tsx
@@ -11,30 +11,12 @@ type IProps = {
 }
 
 export const Board: React.FunctionComponent<IProps> = (props: IProps) => {
-  const NUM_ROWS = 5;
-  const NUM_COLS = 5;
-
-  let rows = [];
-  for (let row = 0; row < NUM_ROWS; row++) {
-    let cells = [];
-    for (let col = 0; col < NUM_COLS; col++) {
-      const pos = (row * NUM_COLS) + col;
-      cells.push(
-        <td key={pos}>
-          <Square square={props.board.squares[pos]} marking={props.markings?.[pos]}
-                  obscured={props.board.obscured} isPrimary={props.isPrimary}/>
-        </td>
-      );
-    }
-    rows.push(<tr key={row}>{cells}</tr>);
-  }
-
-  /* TODO Get rid of this className */
   return (
-    <table className="bingo-table">
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
+    <div className="bingo-board">
+      {props.board.squares.map((s, pos) => (
+        <Square key={pos} square={s} marking={props.markings?.[pos]}
+                obscured={props.board.obscured} isPrimary={props.isPrimary}/>
+      ))}
+    </div>
   );
 }

4b5689b3631247fdc6cc018ce79d4083c525d186
Joshua Prince <joshua@jtprince.com>
2022-04-09 18:33:02 -0700

    Update Easy Server to 1.18 and Paper Download API v2

--- easy-server/INSTALL.sh
+++ easy-server/INSTALL.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || (echo "Failed to find easy-server directory." && exit 1)
 
-PAPER_VERSION=1.16.5
+PAPER_VERSION=1.18
+PAPER_BUILD_FALLBACK=283  # Used if JQ is not installed or latest build check fails
 
 onerror() {
   echo "Failure running install script. Press any key to exit."
@@ -15,7 +16,17 @@ echo "Pulling latest Bingo version..."
 git pull --rebase --autostash
 
 echo "Downloading latest Paper ${PAPER_VERSION} server..."
-curl -sS -o paper.jar https://papermc.io/api/v1/paper/${PAPER_VERSION}/latest/download
+# v2 download API adapted from: https://www.reddit.com/r/admincraft/comments/r20rcf/how_to_download_the_latest_version_of_paper_using/hm45qkx/
+API=https://papermc.io/api/v2
+latest_build="$(curl -sX GET "$API"/projects/paper/version_group/"$PAPER_VERSION"/builds -H 'accept: application/json' | jq '.builds [-1].build' || true)"
+if [ "$latest_build" ]; then
+  echo "Downloading latest Paper build $latest_build."
+else
+  latest_build=PAPER_BUILD_FALLBACK
+  echo "Falling back to Paper build $PAPER_BUILD_FALLBACK."
+fi
+download_url="$API"/projects/paper/versions/"$PAPER_VERSION"/builds/"$latest_build"/downloads/paper-"$PAPER_VERSION"-"$latest_build".jar
+curl -sS -o paper.jar "$download_url"
 
 echo "Checking for Java..."
 if ( ! command -v java ); then
